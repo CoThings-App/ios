c7613a110ab04618e81118b04b1711696932c0cc
Nesim TUNC <nesimtunc@gmail.com>
2020-05-02 00:27:54 +0900

    Initial Commit

---
+++ CoThings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoThings
+//
+//  Created by Neso on 2020/05/01.
+//  Copyright © 2020 Rainlab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# CoThings iOS
+
+[CoThings](https://github.com/rainlab-inc/cothings)'s iOS applications.
+
+Project's progress is [here](https://github.com/rainlab-inc/cothings-ios/projects/1)
+
+#### Stack Versions
+
+Swift: 5.2
+Xcode:14.4.1
+iOS SDK: 13.4
+Min. iOS Support: 13.0+
