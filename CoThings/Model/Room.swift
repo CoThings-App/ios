2362bb849603121022a91f4c3f012907c17b60d5
jz709u <jz709u@gmail.com>
2020-05-05 13:36:57 +0900

    Add Adapter Class around the SwiftPheonix library to make the apis cleaner to deal with.
    cleaned up view controller
    
    added error handling for swift pheonix

--- CoThings/Model/Room.swift
+++ CoThings/Model/Room.swift
@@ -1,16 +1,31 @@
 import Foundation
 
 struct Room: Decodable {
-  let id: Int64
-  let name: String
-  let count: Int64
-  let limit: Int64
-  let group: String
-  let ibeacon_uuid: String?
-  let altbeacon_uuid: String?
-  let major: Int?
-  let minor: Int?
-  let percentage: Int64
-  let css_class: String
-  let last_updated: String
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case count
+        case limit
+        case group
+        case altBeaconUUID = "altbeacon_uuid"
+        case iBeaconUUID = "ibeacon_uuid"
+        case major
+        case minor
+        case percentage
+        case cssClass = "css_class"
+        case lastUpdated = "last_updated"
+    }
+    
+    let id: Int64
+    let name: String
+    let count: Int64
+    let limit: Int64
+    let group: String
+    let iBeaconUUID: String?
+    let altBeaconUUID: String?
+    let major: Int?
+    let minor: Int?
+    let percentage: Int64
+    let cssClass: String
+    let lastUpdated: String
 }
