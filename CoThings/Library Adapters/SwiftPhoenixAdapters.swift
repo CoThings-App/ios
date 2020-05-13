1827fcab16c9627eff51c2103aebf5a671391c17
jz709u <jz709u@gmail.com>
2020-05-13 23:45:03 +0900

    update library adapter

--- CoThings/Library Adapters/SwiftPhoenixAdapters.swift
+++ CoThings/Library Adapters/SwiftPhoenixAdapters.swift
@@ -9,12 +9,12 @@ class MessageSocketAdapter {
         case socketClosed
         case error (Error)
         case socketDisconnect
-        case roomJoin
-        case channelUpdate (MessageAdapter.ChannelState)
-        case channelInc
-        case channelDec
-        case channelJoin (MessageAdapter.ChannelState)
-        case channelPushResponse (MessageAdapter.ChannelState)
+        case roomJoin([Room])
+        case channelUpdate ([Room])
+        case channelInc(Room)
+        case channelDec(Room)
+        case channelJoin ([Room])
+        case channelPushResponseOk
     }
     
     internal enum Action: String {
@@ -89,31 +89,47 @@ class MessageSocketAdapter {
     
     internal func joinChannel(topic: String) {
         let channel = socket.channel(topic, params: ["status": "joining"])
-        channel.delegateOn("join", to: self) { (self, _) in
-            self.socketStatusChanged?(.roomJoin)
-        }
-        
-        channel.delegateOn("update", to: self) { (self, message) in
+        channel.delegateOn("join", to: self) { (self, message) in
+            
             do {
-                let channelState = try MessageAdapter(message: message).decodeResponse()
-                self.socketStatusChanged?(.channelUpdate(channelState))
+                let rooms = try MessageAdapter(message: message).decodeResponse().rooms
+                self.socketStatusChanged?(.roomJoin(rooms))
             } catch {
                 self.socketStatusChanged?(.error(error))
             }
         }
         
-        channel.delegateOn("inc", to: self) { (self, _) in
-            self.socketStatusChanged?(.channelInc)
+        channel.delegateOn("update", to: self) { (self, message) in
+            do {
+                let rooms = try MessageAdapter(message: message).decodeResponse().rooms
+                self.socketStatusChanged?(.channelUpdate(rooms))
+            } catch {
+                self.socketStatusChanged?(.error(error))
+            }
         }
         
-        channel.delegateOn("dec", to: self) { (self, _) in
-            self.socketStatusChanged?(.channelDec)
+        channel.delegateOn("inc", to: self) { (self, message) in
+            do {
+                let room = try MessageAdapter(message: message).decodeRoomStateResponse().room
+                self.socketStatusChanged?(.channelInc(room))
+            } catch {
+                self.socketStatusChanged?(.error(error))
+            }
+        }
+        
+        channel.delegateOn("dec", to: self) { (self, message) in
+            do {
+               let room = try MessageAdapter(message: message).decodeRoomStateResponse().room
+               self.socketStatusChanged?(.channelDec(room))
+            } catch {
+               self.socketStatusChanged?(.error(error))
+            }
         }
         
         channel.join().delegateReceive("ok", to: self) { (self, data) in
             do {
-                let channelState = try MessageAdapter(message: data).decodeResponse()
-                self.socketStatusChanged?(.channelJoin(channelState))
+                let rooms = try MessageAdapter(message: data).decodeResponse().rooms
+                self.socketStatusChanged?(.channelJoin(rooms))
             } catch {
                 self.socketStatusChanged?(.error(error))
             }
@@ -141,12 +157,7 @@ class MessageSocketAdapter {
         }
         
         channel.push(action.rawValue, payload: payload, timeout: 10).receive("ok") { (message) in
-            do {
-               let channelState = try MessageAdapter(message: message).decodeResponse()
-                self.socketStatusChanged?(.channelPushResponse(channelState))
-            } catch {
-               self.socketStatusChanged?(.error(error))
-            }
+            self.socketStatusChanged?(.channelPushResponseOk)
         }.receive("error") { (errorMessage) in
             self.socketStatusChanged?(.error(self.createError(type: .channelReceive,
                                                               userInfo: errorMessage.payload)))
@@ -188,16 +199,33 @@ class MessageAdapter {
         }
         
         let status: String?
-        let rooms: [Room]?
+        let rooms: [Room]
         var action: String?
         
         init(from decoder: Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
             status = try container.decodeIfPresent(String.self, forKey: .status)
             let nestedContainer = try container.nestedContainer(keyedBy: ResponseKeys.self, forKey: .response)
-            rooms = try nestedContainer.decodeIfPresent([Room].self, forKey: .rooms)
+            rooms = try nestedContainer.decode([Room].self, forKey: .rooms)
             
             // TODO: add other state variables
+        }
+    }
+    
+    struct RoomState: Decodable {
+        enum CodingKeys: String, CodingKey {
+            case status
+            case response
+        }
+        
+        let status: String?
+        let room: Room
+        var action: String?
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            status = try container.decodeIfPresent(String.self, forKey: .status)
+            room = try container.decode(Room.self, forKey: .response)
         }
     }
     
@@ -205,6 +233,18 @@ class MessageAdapter {
     
     init(message: Message) {
         self.message = message
+    }
+    
+    internal func decodeRoomStateResponse() throws -> RoomState {
+        do {
+           let jsonData = try JSONSerialization.data(withJSONObject: message.payload, options: .fragmentsAllowed)
+           let decoder = JSONDecoder()
+           var roomResponse = try decoder.decode(RoomState.self, from: jsonData)
+           roomResponse.action = self.message.event
+           return roomResponse
+        } catch {
+           throw NSError(domain: "com.\(self.self)", code: ErrorCodes.improperConversion.rawValue, userInfo: ["info":"\(error.localizedDescription)"])
+        }
     }
     
     internal func decodeResponse() throws -> ChannelState {
