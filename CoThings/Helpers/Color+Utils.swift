8f9121b4ee1383d9f678c0ceba401441a741ccc5
Nesim Tunc <nesimtunc@gmail.com>
2023-10-17 20:30:40 +0900

    Remove Color extension's warning

--- CoThings/Helpers/Color+Utils.swift
+++ CoThings/Helpers/Color+Utils.swift
@@ -11,21 +11,20 @@ import SwiftUI
 extension Color {
     init(hex: String) {
         let scanner = Scanner(string: hex)
-        scanner.scanLocation = 0
         var rgbValue: UInt64 = 0
         scanner.scanHexInt64(&rgbValue)
-
+        
         let r = (rgbValue & 0xff0000) >> 16
         let g = (rgbValue & 0xff00) >> 8
         let b = rgbValue & 0xff
-
+        
         self.init(
             red: Double(r) / 0xff,
             green: Double(g) / 0xff,
             blue: Double(b) / 0xff
         )
     }
-
+    
     static func hex(_ hex: String) -> Color {
         Color(hex: hex)
     }
